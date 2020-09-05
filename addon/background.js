7f178e660bd98b654c3c736758ce19359b114ab9
Arthur Edelstein <arthuredelstein@gmail.com>
2020-09-05 11:26:18 -0700

    cleanup and comment

--- addon/background.js
+++ addon/background.js
@@ -1,7 +1,8 @@
-console.log("hello");
+console.log("Loading dweb-proxy");
 
-// On the request to open a webpage
-const handleProxyRequest = (requestInfo) => {
+// Proxy all dweb (.ipfs) requests via
+// https://dweb.arthuredelstein.net:8500
+browser.proxy.onRequest.addListener((requestInfo) => {
   const url = new URL(requestInfo.url);
   if (url.hostname.endsWith(".ipfs")) {
     let proxyInfo = [{type: "https", host: "dweb.arthuredelstein.net", port: 8500 }];
@@ -9,20 +10,20 @@ const handleProxyRequest = (requestInfo) => {
   } else {
     return [{type: "direct"}];
   }
-}
+}, {urls: ["<all_urls>"]});
 
-browser.proxy.onRequest.addListener(handleProxyRequest, {urls: ["<all_urls>"]});
-
-browser.webRequest.onBeforeRequest.addListener(
-  (request) => {
-    if (request.originUrl === undefined && request.parentFrameId === -1) {
-      let originalUrl = request.url;
-      let originalUrlObject = new URL(originalUrl);
-      let queryEntries = Object.fromEntries(originalUrlObject.searchParams.entries());
-      if (originalUrlObject.host === "www.google.com" &&
-          queryEntries["client"].startsWith("firefox") &&
-          queryEntries["q"].endsWith(".ipfs")) {
-        return { redirectUrl: `http://${queryEntries.q}` };
-      }
+// If user enters example.ipfs domain in the address bar, stop the browser
+// from searching via Google or other search engine, but instead
+// redirect it to http://example.ipfs .
+browser.webRequest.onBeforeRequest.addListener((request) => {
+  if (request.originUrl === undefined && request.parentFrameId === -1) {
+    let originalUrl = request.url;
+    let originalUrlObject = new URL(originalUrl);
+    let queryEntries = Object.fromEntries(originalUrlObject.searchParams.entries());
+    if (originalUrlObject.host === "www.google.com" &&
+        queryEntries["client"].startsWith("firefox") &&
+        queryEntries["q"].endsWith(".ipfs")) {
+      return { redirectUrl: `http://${queryEntries.q}` };
     }
-  }, {"urls":["https://*.google.com/*"]}, ["blocking"]);
+  }
+}, {"urls":["https://*.google.com/*"]}, ["blocking"]);
