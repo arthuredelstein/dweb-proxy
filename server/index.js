2afe23773b69d6fad0dce69b1ce3c74035e48297
Arthur Edelstein <arthuredelstein@gmail.com>
2020-09-05 13:41:47 -0400

    upgrade proxy server to https

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
-const http = require('http');
 const fetch = require('node-fetch');
+const fs = require('fs');
+const https = require('https');
 
-const proxyServer = http.createServer(null, async (req, res) => {
+const options = {
+  key: fs.readFileSync('/home/arthur/certs/dweb.arthuredelstein.net/privkey.pem'),
+  cert: fs.readFileSync('/home/arthur/certs/dweb.arthuredelstein.net/cert.pem')
+};
+
+const proxyServer = https.createServer(options, async (req, res) => {
   console.log("GET", req.url);
+  if (req.url === "/") {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("dweb proxy server");
+  }
   const { host, pathname } = new URL(req.url);
   //console.log(host, pathname);
   let cid32 = host.split(".ipfs")[0];
-  res.writeHead(200, {'Content-Type': 'text/plain'});
   let result = await fetch(`https://cloudflare-ipfs.com/ipfs/${cid32}${pathname}`);
+//  let result = await fetch(`https://ipfs.io/ipfs/${cid32}${pathname}`);
   result.body.pipe(res.socket);
 });
 
