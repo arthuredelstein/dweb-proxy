937cdfce479262c7aec72ea5e085ae0d986c5120
Arthur Edelstein <arthuredelstein@gmail.com>
2020-09-07 01:45:40 -0400

    tabs to spaces

--- server-go/main.go
+++ server-go/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"io"
-	"log"
-	"regexp"
-	"strings"
+  "io"
+  "log"
+  "regexp"
+  "strings"
   "fmt"
   "net/http"
 )
@@ -13,26 +13,26 @@ import (
 
 
 func main() {
-	fmt.Println("starting\n");
+  fmt.Println("starting\n");
   re, _ := regexp.Compile("^([a-z0-9]+)\\.ipfs$")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//spew.Dump(r)
-		//fmt.Println("Host", r.Host)
-		//fmt.Println("RequestURI", r.RequestURI)
-		//fmt.Println("URL", r.URL)
-		//fmt.Println("Header", r.Header)
-		//fmt.Println("Method", r.Method)
-		if strings.HasSuffix(r.Host, ".ipfs") {
-			cidv1 := re.FindStringSubmatch(r.Host)[1]
-			fmt.Println("cidv1", cidv1);
-			gatewayUrl := fmt.Sprintf("https://cloudflare-ipfs.com/ipfs/%s%s", cidv1, r.URL.Path);
-			fmt.Println("gatewayUrl", gatewayUrl);
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    //spew.Dump(r)
+    //fmt.Println("Host", r.Host)
+    //fmt.Println("RequestURI", r.RequestURI)
+    //fmt.Println("URL", r.URL)
+    //fmt.Println("Header", r.Header)
+    //fmt.Println("Method", r.Method)
+    if strings.HasSuffix(r.Host, ".ipfs") {
+      cidv1 := re.FindStringSubmatch(r.Host)[1]
+      fmt.Println("cidv1", cidv1);
+      gatewayUrl := fmt.Sprintf("https://cloudflare-ipfs.com/ipfs/%s%s", cidv1, r.URL.Path);
+      fmt.Println("gatewayUrl", gatewayUrl);
       resp, _ := http.Get(gatewayUrl)
       defer resp.Body.Close()
-			io.Copy(w, resp.Body)
-		}
-	})
+      io.Copy(w, resp.Body)
+    }
+  })
 
-	log.Fatal(http.ListenAndServeTLS(":8500", "/home/arthur/certs/arthuredelstein.net/cert.pem", "/home/arthur/certs/arthuredelstein.net/privkey.pem", nil))
+  log.Fatal(http.ListenAndServeTLS(":8500", "/home/arthur/certs/arthuredelstein.net/cert.pem", "/home/arthur/certs/arthuredelstein.net/privkey.pem", nil))
 }
