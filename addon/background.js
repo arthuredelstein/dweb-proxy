63187c9b4d91eab8eef2979ff7e5a51ffefe530a
Arthur Edelstein <arthuredelstein@gmail.com>
2020-09-12 13:06:05 -0700

    refactor and bump to 0.0.10

--- addon/background.js
+++ addon/background.js
@@ -37,46 +37,51 @@ let updateCurrentProxyInfo = async () => {
   }
 };
 
-if (browser.proxy.onRequest) {
-  // Proxy all dweb (.ipfs) requests via `currentProxyInfo`
-  browser.proxy.onRequest.addListener((requestInfo) => {
-    const url = new URL(requestInfo.url);
-    if (url.hostname.endsWith(".ipfs") || url.hostname.startsWith("ipns.")) {
-      console.log(url.hostname, ":", currentProxyInfo);
+let setupProxying = () => {
+  if (browser.proxy.onRequest) {
+    // Proxy all dweb (.ipfs) requests via `currentProxyInfo`
+    browser.proxy.onRequest.addListener((requestInfo) => {
+      const url = new URL(requestInfo.url);
+      if (url.hostname.endsWith(".ipfs") || url.hostname.startsWith("ipns.")) {
+        console.log(url.hostname, ":", currentProxyInfo);
+        return currentProxyInfo;
+      } else {
+        return [{type: "direct"}];
+      }
       return currentProxyInfo;
-    } else {
-      return [{type: "direct"}];
-    }
-    return currentProxyInfo;
-  }, {urls: ["http://*/*"]});
-}
+    }, {urls: ["http://*/*"]});
+  }
+};
 
 // If user enters example.ipfs domain in the address bar, stop the browser
 // from searching via Google or other search engine, but instead
 // redirect it to http://example.ipfs .
-browser.webRequest.onBeforeRequest.addListener((request) => {
-  if (request.originUrl === undefined && request.parentFrameId === -1) {
-    let originalUrl = request.url;
-    let originalUrlObject = new URL(originalUrl);
-    let { client, sourceid, q } = Object.fromEntries(
-      originalUrlObject.searchParams.entries());
-    if (originalUrlObject.host === "www.google.com" &&
-        ((client && client.startsWith("firefox")) ||
-         (sourceid && sourceid.startsWith("chrome"))) &&
-        (q && q.indexOf(".ipfs") > -1) ||
-        (q && q.indexOf("ipns.") === 0)) {
-      return { redirectUrl: `http://${q}` };
+let setupSearchRedirects = () => {
+  browser.webRequest.onBeforeRequest.addListener((request) => {
+    if (request.originUrl === undefined && request.parentFrameId === -1) {
+      let originalUrl = request.url;
+      let originalUrlObject = new URL(originalUrl);
+      let { client, sourceid, q } = Object.fromEntries(
+        originalUrlObject.searchParams.entries());
+      if (originalUrlObject.host === "www.google.com" &&
+          ((client && client.startsWith("firefox")) ||
+           (sourceid && sourceid.startsWith("chrome"))) &&
+          (q && q.indexOf(".ipfs") > -1) ||
+          (q && q.indexOf("ipns.") === 0)) {
+        return { redirectUrl: `http://${q}` };
+      }
     }
-  }
-}, {"urls":["https://*.google.com/*"]}, ["blocking"]);
-
+  }, {"urls":["https://*.google.com/*"]}, ["blocking"]);
+};
 
 let init = async () => {
   // Connecting to https://arthuredelstein.net ahead of time
   // to make sure we don't see certificate errors in Firefox.
-  await fetch("https://arthuredelstein.net");
-  await updateCurrentProxyInfo();
+  setupProxying();
+  setupSearchRedirects();
   browser.storage.onChanged.addListener(updateCurrentProxyInfo);
+  await updateCurrentProxyInfo();
+  await fetch("https://arthuredelstein.net");
   console.log("init complete");
 };
 
